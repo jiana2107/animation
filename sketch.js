fcc926fd5bfed1694c2986f0a7f53f5321e2b49b
jiana2107 <71509223+jiana2107@users.noreply.github.com>
2020-10-10 11:00:47 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -35,9 +35,9 @@ function setup(){
 function draw(){
     background(0);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+   //console.log(box2.body.position.x);
+    //console.log(box2.body.position.y);
+    //console.log(box2.body.angle);
     box1.display();
     box2.display();
     ground.display();
